0cbcf6c191b30c88d965d6bc6fa77f57e5a2970d
PoorCoder-Here <74219847+PoorCoder-Here@users.noreply.github.com>
2021-06-28 11:04:10 +0530

    Update and rename Crowd_monitoring_using_CV.py to Crowd_monitoring_using_IP.py

--- Crowd_monitoring_using_IP.py
+++ Crowd_monitoring_using_IP.py
@@ -1,40 +1,33 @@
 # import the necessary packages
-from scipy.spatial import distance as dist
 import numpy as np
 import imutils
 import cv2
 import  math
-
 #Social distance config
 # initialize minimum probability to filter weak detections along with
 # the threshold when applying non-maxima suppression
 MIN_CONF = 0.1
 NMS_THRESH = 0.2
-
 # define the minimum safe distance (in pixels) that two people can be
 # from each other
 MIN_DISTANCE = 100
-
 #Detect people function
 def detect_people(frame, net, ln, personIdx=0):
 	# grab the dimensions of the frame and  initialize the list of
 	# results
 	(H, W) = frame.shape[:2]
 	results = []
-
 	# construct a blob from the input frame and then perform a forward
 	# pass of the YOLO object detector, giving us our bounding boxes
 	# and associated probabilities
 	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),swapRB=True, crop=False)
 	net.setInput(blob)
 	layerOutputs = net.forward(ln)
-
 	# initialize our lists of detected bounding boxes, centroids, and
 	# confidences, respectively
 	boxes = []
 	centroids = []
 	confidences = []
-
 	# loop over each of the layer outputs
 	for output in layerOutputs:
 		# loop over each of the detections
@@ -42,13 +35,10 @@ def detect_people(frame, net, ln, personIdx=0):
 			# extract the class ID and confidence (i.e., probability)
 			# of the current object detection
 			scores = detection[5:]
-			#print("scores",scores)
 			classID = np.argmax(scores)
-			#print("Class id",classID)
 			confidence = scores[classID]
-
-			# filter detections by (1) ensuring that the object
-			# detected was a person and (2) that the minimum
+			# filter detections by ensuring that the object
+			# detected was a person and that the minimum
 			# confidence is met
 			if classID == personIdx and confidence > MIN_CONF:
 				# scale the bounding box coordinates back relative to
@@ -58,22 +48,18 @@ def detect_people(frame, net, ln, personIdx=0):
 				# height
 				box = detection[0:4] * np.array([W, H, W, H])
 				(centerX, centerY, width, height) = box.astype("int")
-
 				# use the center (x, y)-coordinates to derive the top
 				# and and left corner of the bounding box
 				x = int(centerX - (width / 2))
 				y = int(centerY - (height / 2))
-
 				# update our list of bounding box coordinates,
 				# centroids, and confidences
 				boxes.append([x, y, int(width), int(height)])
 				centroids.append((centerX, centerY))
 				confidences.append(float(confidence))
-
 	# apply non-maxima suppression to suppress weak, overlapping
 	# bounding boxes
 	idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
-
 	# ensure at least one detection exists
 	if len(idxs) > 0:
 		# loop over the indexes we are keeping
@@ -81,63 +67,49 @@ def detect_people(frame, net, ln, personIdx=0):
 			# extract the bounding box coordinates
 			(x, y) = (boxes[i][0], boxes[i][1])
 			(w, h) = (boxes[i][2], boxes[i][3])
-
 			# update our results list to consist of the person
 			# prediction probability, bounding box coordinates,
 			# and the centroid
 			r = (confidences[i], (x, y, x + w, y + h), centroids[i])
 			results.append(r)
-
 	# return the list of results
 	return results
-
 # load the COCO class labels our YOLO model was trained on
-labelsPath = "C:\\Users\\bhuva\\PycharmProjects\\Crowd monitor system\\Crowd monitoring system using CV\\yolo-coco\\coco.names"
+labelsPath = "yolo-coco\\coco.names"
 LABELS = open(labelsPath).read().strip().split("\n")
-
 # derive the paths to the YOLO weights and model configuration
-weightsPath = "C:\\Users\\bhuva\\PycharmProjects\\Crowd monitor system\\Crowd monitoring system using CV\\yolo-coco\\yolov3.weights"
-configPath = "C:\\Users\\bhuva\\PycharmProjects\\Crowd monitor system\\Crowd monitoring system using CV\\yolo-coco\\yolov3.cfg"
-
+weightsPath = "yolo-coco\\yolov3.weights"
+configPath = "yolo-coco\\yolov3.cfg"
 # load our YOLO object detector trained on COCO dataset (80 classes)
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
-
 # determine only the *output* layer names that we need from YOLO
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
 # initialize the video stream and pointer to output video file
 print("[INFO] accessing video stream...")
-#vs = cv2.VideoCapture(args["input"] if args["input"] else 0)
+# video footage
 vs = cv2.VideoCapture("Shopping, People, Commerce, Mall, Many, Crowd, Walking   Free Stock video footage   YouTube.mp4")
-
 # loop over the frames from the video stream
 while True:
 	# read the next frame from the file
 	(grabbed, frame) = vs.read()
-
 	# if the frame was not grabbed, then we have reached the end
 	# of the stream
 	if not grabbed:
 		break
-
 	# resize the frame and then detect people (and only people) in it
 	frame = imutils.resize(frame, width=900)
 	results = detect_people(frame, net, ln,personIdx=LABELS.index("person"))
-
 	# initialize the set of indexes that violate the minimum social
 	# distance
 	violate = set()
-
-	# ensure there are *at least* two people detections (required in
+	# ensure there are *at least* five people detections (required in
 	# order to compute our pairwise distance maps)
 	if len(results) >= 5:
 		# extract all centroids from the results and compute the
 		# Euclidean distances between all pairs of the centroids
 		centroids = np.array([r[2] for r in results])
-		#print("Centrods",centroids)
-
 		i=0
 		while i<len(centroids):
 			violate_pairs = []
@@ -146,7 +118,6 @@ while True:
 			while j<len(centroids):
 				if i!=j:
 					y=centroids[j]
-					#print(x,y)
 					dis = math.sqrt((x[0]-y[0])**2 + (x[1]-y[1])**2)
 					if dis<MIN_DISTANCE:
 						violate_pairs.append(j)
@@ -154,49 +125,27 @@ while True:
 			violate_pairs.append(i)
 			if len(violate_pairs)>=4:
 				for w in violate_pairs:
-					#print("w",w)
 					violate.add(w)
 			i+=1
-
-
-		"""D = dist.cdist(centroids, centroids, metric="euclidean")
-		# loop over the upper triangular of the distance matrix
-		for i in range(0, D.shape[0]):
-			for j in range(i + 1, D.shape[1]):
-				# check to see if the distance between any two
-				# centroid pairs is less than the configured number
-				# of pixels
-				if D[i, j] < MIN_DISTANCE:
-					# update our violation set with the indexes of
-					# the centroid pairs
-					print("i,j",i,j)
-					violate.add(i)
-					violate.add(j)"""
-
 	# loop over the results
 	for (i, (prob, bbox, centroid)) in enumerate(results):
 		# extract the bounding box and centroid coordinates, then
 		# initialize the color of the annotation
-		#print("i",i)
 		(startX, startY, endX, endY) = bbox
 		(cX, cY) = centroid
 		color = (204, 204, 0)
-
 		# if the index pair exists within the violation set, then
 		# update the color
 		if i in violate:
 			color = (0, 255, 255)
-
-		# draw (1) a bounding box around the person and (2) the
+		# draw a bounding box around the person and (2) the
 		# centroid coordinates of the person,
 		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 		cv2.circle(frame, (cX, cY), 5, color, 1)
-
 	# draw the total number of social distancing violations on the
 	# output frame
 	text = "Crowded areas: {}".format(int(len(violate)/4))
 	cv2.putText(frame, text, (10, frame.shape[0] - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 255, 255), 3)
-
 	# output frame should be displayed to our screen
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
